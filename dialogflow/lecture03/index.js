ef9340553ffd2c117294da3854b7d173998ca937
Jungwoon Lee <jungwoonlee@jungwoonlee-macbookpro2.roam.corp.google.com>
2018-07-31 18:00:02 +0900

    lecture02

---
+++ dialogflow/lecture03/index.js
@@ -0,0 +1,82 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+}
+
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+   // Uncomment and edit to make your own intent handler
+   // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+   // below to get this function to be run when a Dialogflow intent is matched
+   function storeFunctionHandler(agent) {
+     const allContext = agent.contexts;
+     const context = agent.getContext('00010-problemintent-followup');
+     console.log('context : ' + JSON.stringify(allContext));
+     agent.add(`수리를 요청한 프로덕트는 ` + context.parameters.productname +
+     ' 이며 예약일자는 ' + context.parameters["date-time"].date_time + '입니다.');
+     //agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+   }
+  
+   function callAskDate(agent) {
+     const context = agent.getContext('00010-problemintent-followup');
+     //agent.setContext("00010-problemintent-followup");
+     //agent.setFollowupEvent({ "name": "ask_date", "parameters":{"productname": context.parameters.productname}, "languageCode": "ko"} );
+     agent.setFollowupEvent({ "name": "ask_date", "parameters":{"productname": context.parameters.productname}} );
+     //agent.setFollowupEvent("ask_date");
+   }
+  
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('00010-problemIntent - custom - yes', storeFunctionHandler);
+  intentMap.set('00010-problemIntent - custom - no', callAskDate);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
